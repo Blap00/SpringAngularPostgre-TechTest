d5e23f5e6f7d6ff861b509d7bc6d32290f4b2183
Blap00 <fa.palma@duocuc.cl>
2024-04-25 17:43:26 -0400

    FIX Backend

--- techTest/TechTest/TechTest/src/main/java/com/fabianpalma/techtest/models/Charge_Points.java
+++ techTest/TechTest/TechTest/src/main/java/com/fabianpalma/techtest/models/Charge_Points.java
@@ -1,10 +1,14 @@
 package com.fabianpalma.techtest.models;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "charge_points")
 public class Charge_Points {
 	//GENERATE ID From SPRING
 	@Id
@@ -43,6 +47,7 @@ public class Charge_Points {
 	
 	// Constructors
     public Charge_Points() {
+    	//VOID
     }
 
     public Charge_Points(String status, String operator, Integer connections, String latitude, String longitude, String country, Integer power) {
--- techTest/TechTest/TechTest/src/main/java/com/fabianpalma/techtest/repositories/Charge_PointsRepository.java
+++ techTest/TechTest/TechTest/src/main/java/com/fabianpalma/techtest/repositories/Charge_PointsRepository.java
@@ -1,8 +1,25 @@
 package com.fabianpalma.techtest.repositories;
 
-import com.fabianpalma.techtest.models.Charge_Points;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import com.fabianpalma.techtest.models.Charge_Points;
+
+@Repository
 public interface Charge_PointsRepository extends JpaRepository<Charge_Points, Long> {
-		
+
+    @Query(value = "SELECT *, "
+            + "ACOS(SIN(RADIANS(?1)) * SIN(RADIANS(latitude)) + "
+            + "COS(RADIANS(?1)) * COS(RADIANS(latitude)) * COS(RADIANS(?2) - RADIANS(longitude))) "
+            + "* 6371 AS distance "
+            + "FROM electromobility.charge_points "
+            + "ORDER BY distance ASC "
+            + "LIMIT 1", nativeQuery = true)
+    Charge_Points findNearestChargePoint(double latitude, double longitude);
+	    
+	
+    @Query("SELECT c, c.power FROM Charge_Points c ORDER BY c.power DESC LIMIT 1")
+    Charge_Points findChargePointWithMaxPower();
+
 }
